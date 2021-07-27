65a01b9e303741c8e3c831d41290ecfd7bce93f4
adobias7811 <adobias11@gmail.com>
2021-07-27 16:42:15 -0400

    Added footer with links to portfolio & GitHub

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> React Weather App
+    <footer>This project was coded by {" "}<a href="ttps://silly-kepler-6e4998.netlify.app" target="_blank" rel="noreferrer">Amanda Dobias</a> and is {" "}<a href="https://github.com/adobias7811/react-weather-app" target="_blank" rel="noreferrer">open sourced on GitHub</a></footer>
     </div>
   );
 }
