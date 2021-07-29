ac79d24e3bc0f827e975425649a8c11e44fdaadb
adobias7811 <adobias11@gmail.com>
2021-07-29 09:00:41 -0400

    Added Bootstrap to application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App"> React Weather App
-    <footer>This project was coded by {" "}<a href="ttps://silly-kepler-6e4998.netlify.app" target="_blank" rel="noreferrer">Amanda Dobias</a> and is {" "}<a href="https://github.com/adobias7811/react-weather-app" target="_blank" rel="noreferrer">open sourced on GitHub</a></footer>
+    <div className="App">
+      {" "}
+      React Weather App
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="ttps://silly-kepler-6e4998.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amanda Dobias
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/adobias7811/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
